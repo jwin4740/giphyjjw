b2b10c06517c20453b4ba1e722ea23a55f29fbc4
James <jwin4740@gmail.com>
2017-01-13 12:45:05 -0500

    final review

--- assets/javascript/jscript.js
+++ assets/javascript/jscript.js
@@ -35,14 +35,13 @@ $(document).ready(function() {
         console.log(topicsArray);
     });
 
-     $("#search").on("keypress", function() {
-      if (event.keyCode === 13)
-      {
-        var tvshow = $("#searchvalue").val().trim();
-        topicsArray.push(tvshow);
-        insertButtons();
-        console.log(topicsArray);
-      }
+    $("#search").on("keypress", function() {
+        if (event.keyCode === 13) {
+            var tvshow = $("#searchvalue").val().trim();
+            topicsArray.push(tvshow);
+            insertButtons();
+            console.log(topicsArray);
+        }
     });
 
 
@@ -60,7 +59,7 @@ $(document).ready(function() {
         })
 
         .done(function(response) {
-          console.log(response);
+            console.log(response);
             var responseLength = response.data.length;
             console.log(queryURL);
             $("#showgifs").empty()
@@ -70,19 +69,24 @@ $(document).ready(function() {
                 $("#responseMessage").html("Your search only returned " + responseLength + " results");
             }
 
-             if (responseLength === 0) {
+            if (responseLength === 0) {
                 $("#responseMessage").html("Your search did not return any results");
             }
 
+            // var testimage = "http://media0.giphy.com/media/Fr9rgAhT9JIw8/giphy.gif";
+            // testimage = $("<img>");
+            // testimage.attr({ "class" : "gifs" });
+            // $("#showgifs").append(testimage);
+
 
             for (var i = 0; i < responseLength; i++) {
-                imageUrl = response.data[i].images.original.url;
+                imageUrl = response.data[i].images.original_still.url;
+                var imageUrlAnimate = response.data[i].images.original.url;
+                var imageUrlStill = response.data[i].images.original_still.url;
                 rating = response.data[i].rating;
                 picDiv = $("<div class='dyno dynadiv" + i + "'> <h3 class='rating'> Rating: " + rating + "</h3></div>");
                 image = $("<img>");
-                image.attr("class", "gifs");
-                image.attr("src", imageUrl);
-                image.attr("alt", "tv image");
+                image.attr({ "class": "gifs", "src": imageUrl, "data-still": imageUrlStill, "data-animate": imageUrlAnimate, "data-state" : "still", "alt": "tvgiphy" });
                 $("#showgifs").append(picDiv);
                 $(".dynadiv" + i).append(image);
 
@@ -92,7 +96,20 @@ $(document).ready(function() {
     });
 
 
-
+    $("#showgifs").on("click", ".gifs", function() {
+        // The attr jQuery method allows us to get or set the value of any attribute on our HTML element
+        var state = $(this).attr("data-state");
+        // If the clicked image's state is still, update its src attribute to what its data-animate value is.
+        // Then, set the image's data-state to animate
+        // Else set src to the data-still value
+        if (state === "still") {
+            $(this).attr("src", $(this).attr("data-animate"));
+            $(this).attr("data-state", "animate");
+        } else {
+            $(this).attr("src", $(this).attr("data-still"));
+            $(this).attr("data-state", "still");
+        }
+    });
 
 
 
